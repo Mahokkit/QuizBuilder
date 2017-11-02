a4def147faf8b85487e6b3e697261edf52f34ce8
Edward Ma <ma.edward@hotmail.com>
2017-11-02 08:45:49 -0300

    Bug:
    repeatable answers, and it will crash after a while.

--- app/src/main/java/com/example/edward/quizbuilder/ActivityThree.java
+++ app/src/main/java/com/example/edward/quizbuilder/ActivityThree.java
@@ -1,28 +1,41 @@
 package com.example.edward.quizbuilder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ActivityThree extends AppCompatActivity
 {
-    TextView score = findViewById(R.id.scoreView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
-
+        Button againBtn = findViewById(R.id.againBtn);
         Bundle toThree = getIntent().getExtras();
 
         String fromBundle;
 
         if (toThree != null) //exist
         {
+            TextView score = findViewById(R.id.scoreView);
             fromBundle = toThree.getString("SCORE");
-            this.score.setText("You got " + fromBundle.toString() + " out of 10!");
+            score.setText("You got " + fromBundle.toString() + " out of 10!");
         }
-
+        againBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = getBaseContext().getPackageManager()
+                        .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
     }
 }
